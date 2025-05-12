9ece87b9b02bc1b1eddd65f4afa900983f7539f3
tpham93 <tuan.pham@uni-kassel.de>
2025-05-12 16:39:17 +0200

    Remove test case that is incompatible for joblib>=1.5

--- skactiveml/pool/tests/test_wrapper.py
+++ skactiveml/pool/tests/test_wrapper.py
@@ -309,7 +309,8 @@ class TestParallelUtilityEstimationWrapper(
             ({"backend": "threading"}, None),
             ({"backend": "loky", "batch_size": 2}, None),
             ({"backend": "loky", "batch_size": 2, "n_jobs": 1}, None),
-            ({"abcdefg": "test"}, TypeError),
+            # This case no longer applies to joblib>=1.5
+            # ({"abcdefg": "test"}, TypeError),
             (0, TypeError),
             ("multi", TypeError),
             ([0], TypeError),
